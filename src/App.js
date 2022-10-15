2816b6eef97ba387f3abff2b85592d72b9138108
Andras korosi <89639857+Drewcamper@users.noreply.github.com>
2022-10-15 16:55:06 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import React from 'react'
+import { WeatherDataPage } from './components/WeatherDataPage'
+import { WeatherDataProvider } from './context/context'
+
+
+
+function App() {
+
+  return  ( 
+    <WeatherDataProvider>
+      <WeatherDataPage />
+    </WeatherDataProvider>
+  )
+}
+
+
+export default App
